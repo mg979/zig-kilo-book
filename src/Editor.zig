d90b3be95d65fb43390f9dddb36070a2e536dda7
Gianmaria Bajo <gianmariabajo@proton.me>
2025-09-05 00:12:08 +0200

    12.3 Move with keys

--- src/Editor.zig
+++ src/Editor.zig
@@ -243,6 +243,52 @@ fn processKeypress(e: *Editor) !void {
 
 ///////////////////////////////////////////////////////////////////////////////
 //
+//                              View operations
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/// Update the cursor position after a key has been pressed.
+fn moveCursorWithKey(e: *Editor, key: t.Key) void {
+    const V = &e.view;
+    const numrows = e.buffer.rows.items.len;
+
+    switch (key) {
+        .left => {
+            if (V.cx != 0) { // not the first column
+                V.cx -= 1;
+            }
+            else if (V.cy > 0) { // move back to the previous row
+                V.cy -= 1;
+                V.cx = e.currentRow().clen();
+            }
+        },
+        .right => {
+            if (V.cy < numrows) {
+                if (V.cx < e.currentRow().clen()) { // not the last column
+                    V.cx += 1;
+                }
+                else { // move to the next row
+                    V.cy += 1;
+                    V.cx = 0;
+                }
+            }
+        },
+        .up => {
+            if (V.cy != 0) {
+                V.cy -= 1;
+            }
+        },
+        .down => {
+            if (V.cy < numrows) {
+                V.cy += 1;
+            }
+        },
+        else => {},
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
 //                              Screen update
 //
 ///////////////////////////////////////////////////////////////////////////////
