559928ceb0b159122a76ff3049636121ced15eaa
Gianmaria Bajo <gianmariabajo@proton.me>
2025-09-05 05:09:05 +0200

    17. Highlight

--- src/ansi.zig
+++ src/ansi.zig
@@ -173,6 +173,48 @@ pub const BgDefault = CSI ++ "40m";
 /// Foreground color
 pub const FgDefault = CSI ++ "39m";
 
+/// Codes for 16-colors terminal escape sequences (foreground)
+pub const FgColor = struct {
+    pub const default: u8 = 39;
+    pub const black: u8 = 30;
+    pub const red: u8 = 31;
+    pub const green: u8 = 32;
+    pub const yellow: u8 = 33;
+    pub const blue: u8 = 34;
+    pub const magenta: u8 = 35;
+    pub const cyan: u8 = 36;
+    pub const white: u8 = 37;
+    pub const black_bright: u8 = 90;
+    pub const red_bright: u8 = 91;
+    pub const green_bright: u8 = 92;
+    pub const yellow_bright: u8 = 93;
+    pub const blue_bright: u8 = 94;
+    pub const magenta_bright: u8 = 95;
+    pub const cyan_bright: u8 = 96;
+    pub const white_bright: u8 = 97;
+};
+
+/// Codes for 16-colors terminal escape sequences (background)
+pub const BgColor = struct {
+    pub const default: u8 = 49;
+    pub const black: u8 = 40;
+    pub const red: u8 = 41;
+    pub const green: u8 = 42;
+    pub const yellow: u8 = 43;
+    pub const blue: u8 = 44;
+    pub const magenta: u8 = 45;
+    pub const cyan: u8 = 46;
+    pub const white: u8 = 47;
+    pub const black_bright: u8 = 100;
+    pub const red_bright: u8 = 101;
+    pub const green_bright: u8 = 102;
+    pub const yellow_bright: u8 = 103;
+    pub const blue_bright: u8 = 104;
+    pub const magenta_bright: u8 = 105;
+    pub const cyan_bright: u8 = 106;
+    pub const white_bright: u8 = 107;
+};
+
 /// Hide the terminal cursor
 pub const HideCursor = CSI ++ "?25l";
 
